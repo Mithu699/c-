55ff9cbe99edb87acd0445c06bc472d3f6e0ceb0
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-09-12 11:33:44 +0530

    Add files via upload

---
+++ 02-primitive/datatypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float teaPrice = 49.99;
+    int roundedprice = (int) teaPrice;
+
+
+    int teaQuantitiy = 2;
+     double totalprice = teaPrice * teaQuantitiy;
+
+    cout << totalprice << endl;
+
+return 0;
+
+}
