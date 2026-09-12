14eb4a35086c6a79f97ac0fd54e28c32511064de
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-09-12 11:34:19 +0530

    Add files via upload

---
+++ 02-primitive/modifier.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned smallTeaPack = -100;
+    double largeTeaStorage = 1000000000; // use long for storing large amount of number in cpp
+    short TeaSample = 43;
+    
+    cout << largeTeaStorage << endl;
+}
+
+// unsigned agr likha hai to uska mtlb negative value nhi hogi
+// zyada documentary pdhna hai to microsoft ka search kr lena 
+
